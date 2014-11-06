e8b10a1603a22c5f40fe877d1e5c9243bb83dcd3
Daniel Chen <chen.daniel7@gmail.com>
2014-11-06 01:23:59 -0500

    Gets weather and sends text

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-PythonWeather
-=============
-
-A simple script that gets the weather based on a zipcode and sends it to a phone using Twilio
+A simple app that accepts a zip code and looks up the city using Google Map's Api.
+The city is then passed into openweather's API and the weather info is sent to a phone through Twilio
---
+++ test.py
@@ -0,0 +1,29 @@
+import urllib2
+import json
+from twilio.rest import TwilioRestClient
+
+google_url = "http://maps.googleapis.com/maps/api/geocode/json?address="
+weather_url = "http://api.openweathermap.org/data/2.5/weather?q="
+account_sid = "AC6e810f461c59369afc89d751e0a60129"
+auth_token = "4c12e1dd30e30177552bc9bc37a4ae59"
+client = TwilioRestClient(account_sid, auth_token)
+
+def send_sms(zip_code, number):
+	response = urllib2.urlopen(google_url + str(zip_code)).read()
+	json_response = json.loads(response)
+	city_name = json_response["results"][0]["formatted_address"]
+	print city_name
+	response2 = urllib2.urlopen(weather_url + city_name.replace(' ', '%20')).read()
+	json_response2 = json.loads(response2)
+	weather = json_response2["weather"][0]
+	weather_desc = weather["main"] + " | " + weather["description"]
+	print weather_desc
+
+	message = client.messages.create(
+		body = weather_desc,
+		to = number,
+		from_ = "+12407433765"
+	)
+
+
+send_sms(20742, 2406717497)
